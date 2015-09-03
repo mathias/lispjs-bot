ccce29885c80c27e30c18db64e29c3711fd6cea2
Matt Gauger <matt.gauger@gmail.com>
2015-09-02 21:19:19 -0500

    On invite, join only channels in CHANNEL_LIST
    - Extract out logger function rather than println. Will make expanding
      logging easier later.
    - Simplify env var access.

--- src/lispjs_bot/core.cljs
+++ src/lispjs_bot/core.cljs
@@ -6,15 +6,15 @@
 (def irc (node/require "irc"))
 
 ;; ENV vars
-(defn env []
+(def env
   (.-env js/process))
 
-(def irc-server (or (.-IRC_SERVER (env))
+(def irc-server (or (.-IRC_SERVER env)
                     "irc.freenode.org"))
-(def username (or (.-USERNAME (env))
+(def username (or (.-USERNAME env)
                   "lispjs"))
-(def password (.-PASSWORD (env)))
-(def channel-list (let [chan-str (.-CHANNEL_LIST (env))]
+(def password (.-PASSWORD env))
+(def channel-list (let [chan-str (.-CHANNEL_LIST env)]
                     (.split chan-str ",")))
 
 ;; IRC client
@@ -28,15 +28,20 @@
 (defn add-listener [event callback]
   (.addListener client event callback))
 
+(defn logger [ctx msg]
+  (println ctx ": " msg))
+
 (add-listener "error"
               (fn [msg]
-                (println "error: " msg)))
+                (logger "error" msg)))
 
 
 (add-listener "message"
               (fn [from room message]
-                (println room " | " from ":" message)
-                (when (re-seq #"^[#]" room)
+                (logger (str room " | " from) message)
+
+                (when (and (re-seq #"^[#]" room)
+                           (re-seq #"^!lispjs" message))
                   ;; Room notified, not PM
                   (cond
                    (re-seq #"dance" message) (.say client room "\u0001ACTION dances: :D\\-<\u0001")))))
@@ -44,26 +49,29 @@
 (add-listener "motd"
               (fn [msg]
                 ;; msg nickserv to log in
-                (println "Identifying..")
+                (logger "motd" msg)
+
                 (.say client
                       "NickServ"
                       (str "IDENTIFY " username " " password))
                 (.whois client username
-                        (fn [from to message]
-                          (println "whois " from to message)))))
+                        (fn [response]
+                          (logger "whois" response)))))
 
 (add-listener "invite"
               (fn [channel by]
-                (println "Invited to " channel " by " by)
-                (.join client channel)))
+                (logger "invite" (str "Invited to " channel " by " by))
+                (if (some #{channel} channel-list)
+                  (.join client channel)
+                  (logger "Warning" (str "Invited to channel " channel " by " by " but " channel " is not in channel list!")))))
 
 (add-listener "notice"
               (fn [from to msg]
-                (println "* " from "> " to ": " msg)))
+                (logger (str (or from "") "> " to) msg)))
 
 (add-listener "topic"
               (fn [channel topic nick]
-                (println channel " | " "Topic: " topic)))
+                (logger "topic" (str channel "> " topic))))
 
 (defn -main [& args]
   (println "Starting up lispjs"))
